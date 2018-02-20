9354ee55160095f1aaffd06ea2ac6bc27e0f2afb
minauteur <Thompennington@gmail.com>
2018-02-20 14:51:19 -0600

    MVP commit

---
+++ src/Loopr.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+export class Loopr extends Component {
+    constructor() {
+        super();
+        this.state = { items: [] };
+    }
+    componentDidMount() {
+        fetch('http://localhost:8080') // returns a promise object
+        .then(result => result.json()) // still returns a promise object, U need to chain it again
+        .then(items => this.setState({items}));
+    }
+    render() {
+        return (
+            <div className="Container">
+                <ul>
+                    {this.state.items.map(item=><li key={item.name}>{item.name} <a href={item.url}><img src={item.img}></img></a></li>)}
+                </ul>
+            </div>
+        );
+    }
+
+}
+export default Loopr;
