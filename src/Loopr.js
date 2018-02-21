a3a5ac8d53c6d16d3ef0e312bf21331613f5926b
minauteur <Thompennington@gmail.com>
2018-02-21 09:41:34 -0600

    updates fetch logic

--- src/Loopr.js
+++ src/Loopr.js
@@ -5,7 +5,7 @@ export class Loopr extends Component {
         this.state = { items: [] };
     }
     componentDidMount() {
-        fetch('http://localhost:8080') // returns a promise object
+        fetch('https://microwavemansion.com/loopinfo') // returns a promise object
             .then(result => result.json()) // still returns a promise object, U need to chain it again
             .then(items => this.setState({items}));
     }
