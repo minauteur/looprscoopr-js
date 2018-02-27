de915147a536e5f4a9618d4e32c8fafaea65b38d
minauteur <Thompennington@gmail.com>
2018-02-26 20:35:57 -0600

    updates for deployment

--- src/App.js
+++ src/App.js
@@ -7,10 +7,6 @@ class App extends Component {
     render() {
         return ( 
             <div className = "App">
-                <header className = "App-header">
-                    <img src = {logo} className = "App-logo" alt = "logo"/>
-                    <h1 className = "App-title"> LooprScoopr</h1>   
-                </header>
                     <p className = "App-intro">
                         now serving microwavemansion.com/loops!
                     </p> 
